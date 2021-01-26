ea56770ff7e983f66b401e7d11767648fb514ae7
Jorge <jorgicoor1998@gmail.com>
2021-01-26 22:44:36 +0100

    improves

--- comunes/footer.php
+++ comunes/footer.php
@@ -2,7 +2,7 @@
 
 	<div id="copyright">
 		<p>
-		Copyright 2021 //Construcción de Servicios de Información Digital
+		© Copyright 2021 //Construcción de Servicios de Información Digital
 		</p>
 	</div>
 
